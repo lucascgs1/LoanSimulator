9ace70dc2c6675ba754f35f55a9b269a8746549c
Lucas Coutinho <lucascgs1@hotmail.com>
2023-06-19 15:52:48 -0300

    feat(simulacao-emprestimo): desabilitar campo ao simular emprestimo

--- src/app/pages/simulation/component/simulation-form/simulation-form.component.ts
+++ src/app/pages/simulation/component/simulation-form/simulation-form.component.ts
@@ -4,7 +4,6 @@ import { LoanRequest } from '../../../../core/model/loan/loan-request';
 // package
 import { Component, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
 import { EventEmitter } from '@angular/core';
 
 @Component({
